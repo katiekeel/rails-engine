31bfc0d9a955c4845fc9525cc21b6c7aee5d33d4
Katie Keel <akatiekeel@gmail.com>
2017-08-14 16:40:08 -0600

    Merchant model and factory

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
   factory :customer do
-    first_name "MyString"
-    last_name "MyString"
+    sequence :first_name do |x|
+      "Customer #{x}"
+    end
+
+    sequence :last_name do |x|
+      "Lastname #{x}"
+    end
   end
 end
