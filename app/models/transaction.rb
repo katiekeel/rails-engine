c574f83775b6f2264048f374fad02087453a733d
Katie Keel <akatiekeel@gmail.com>
2017-08-17 15:28:13 -0600

    Favorite merchant for customer passing

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,7 @@ class Transaction < ApplicationRecord
   belongs_to :invoice
   has_many :invoice_items, through: :invoice
   has_one :customer, through: :invoice
+  has_one :merchant, through: :invoice
 
   enum result: ["success", "failed"]
 
--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,7 @@ Rails.application.routes.draw do
         get "/random.json", to: "random#show"
         get "/:id/transactions", to: "transactions#index"
         get "/:id/invoices", to: "invoices#index"
+        get "/:id/favorite_merchant", to: "favorite_merchant#show"
       end
       resources :customers, only: [:index, :show]
         get "/:id/revenue", to: "revenue#show"
