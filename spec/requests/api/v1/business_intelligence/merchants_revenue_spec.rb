2b4afc9171ecf39d6a57f28e524e6e258ddff75b
Katie Keel <akatiekeel@gmail.com>
2017-08-17 09:54:53 -0600

    Individual merchant revenue by date test done

--- spec/requests/api/v1/business_intelligence/merchants_revenue_spec.rb
+++ spec/requests/api/v1/business_intelligence/merchants_revenue_spec.rb
@@ -13,4 +13,17 @@ describe "Revenue for a single merchant API" do
     json = JSON.parse(response.body)
     expect(json["revenue"]).to eq "18.0"
   end
+
+  scenario "returns the revenue for a specific date" do
+    date = "2012-03-16 11:55:05"
+    merchant = create(:merchant)
+    invoice = create(:invoice, :with_invoice_items, :with_transactions)
+    invoice.update(created_at: date)
+    merchant.invoices << invoice
+
+    get "/api/v1/merchants/#{merchant.id}/revenue?date=#{date}"
+
+    json = JSON.parse(response.body)
+    expect(json["revenue"]).to eq "6.0"
+  end
 end
